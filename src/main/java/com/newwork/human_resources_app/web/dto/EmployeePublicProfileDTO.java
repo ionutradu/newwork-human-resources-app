b20f8c2f3db982fbe614e9f2a217179c95e8a431
Ionut Radu <ionut.radu.ext@adamos.com>
2025-12-08 13:53:44 +0200

    Fix employee public DTO id

--- src/main/java/com/newwork/human_resources_app/web/dto/EmployeePublicProfileDTO.java
+++ src/main/java/com/newwork/human_resources_app/web/dto/EmployeePublicProfileDTO.java
@@ -4,8 +4,10 @@ import lombok.Data;
 
 @Data
 public class EmployeePublicProfileDTO {
-    private Long id;
+
+    private String id;
     private String firstName;
     private String lastName;
     private String jobTitle;
+
 }
